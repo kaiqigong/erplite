d0f3cb501fc83375527a26d9192294b33aee2217
lacusrinz <lacusrinz@gmail.com>
2014-03-23 22:59:54 +0800

    Add models

--- Server/Contacts/models.py
+++ Server/Contacts/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+class Contacts(models.Model):
+	name = models.CharField(max_length=255)
+	avator = models.URLField(blank=True)
+	description = models.TextField(blank=True)
+	createdDate = models.DateField(auto_now_add=True)
+	createdBy = models.CharField(max_length=100)
+	modifiedDate = models.DateField(auto_now=True)
+	modifiedBy = models.CharField(max_length=100)
+
+class ContactTag(models.Model):
+	contact = models.ForeignKey('Contacts')
+	tag = models.CharField(max_length=100)
+	createdDate = models.DateField(auto_now_add=True)
+	createdBy = models.CharField(max_length=100)
+	modifiedDate = models.DateField(auto_now=True)
+	modifiedBy = models.CharField(max_length=100)
+
+class ContactData(models.Model):
+	contact = models.ForeignKey('Contacts')
+	key = models.TextField()
+	value = models.TextField()
+	createdDate = models.DateField(auto_now_add=True)
+	createdBy = models.CharField(max_length=100)
+	modifiedDate = models.DateField(auto_now=True)
+	modifiedBy = models.CharField(max_length=100)
+
+class ContactLink(models.Model):
+	contact = models.ForeignKey('Contacts')
+	entityType = models.TextField()
+	entity = models.ForeignKey('Entity')
+	createdDate = models.DateField(auto_now_add=True)
+	createdBy = models.CharField(max_length=100)
+	modifiedDate = models.DateField(auto_now=True)
+	modifiedBy = models.CharField(max_length=100)
+
+# class ContactComment(models.Model):
+# 	contact = ForeignKey('Contacts')
+# 	comment = TextField()
+# 	author = ManyToManyField()
+# 	createdDate = models.DateField(auto_now_add=True)
+# 	createdBy = models.CharField(max_length=100)
+# 	modifiedDate = models.DateField(auto_now=True)
+# 	modifiedBy = models.CharField(max_length=100)
+
+
+
+class Entity(models.Model):
+	name = models.TextField()
+	createdDate = models.DateField(auto_now_add=True)
+	createdBy = models.CharField(max_length=100)
+	modifiedDate = models.DateField(auto_now=True)
+	modifiedBy = models.CharField(max_length=100)
--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'erplit',                      # Or path to database file if using sqlite3.
+        'NAME': 'erplite',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'password',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'Contacts',
 )
 
 # A sample logging configuration. The only tangible logging
