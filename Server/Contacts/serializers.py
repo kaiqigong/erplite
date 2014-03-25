e797c8152fd7031a7d4898f01ecd6b6096f69705
lacusrinz <lacusrinz@gmail.com>
2014-03-25 22:36:46 +0800

    Fix: contacttag post failed

--- Server/Contacts/serializers.py
+++ Server/Contacts/serializers.py
@@ -2,24 +2,27 @@ from rest_framework import serializers
 from Contacts.models import Contacts, ContactTag, ContactData, ContactLink
 
 class ContactsSerializer(serializers.ModelSerializer):
+	tags = serializers.RelatedField(many=True)
+	data = serializers.RelatedField(many=True)
+	links = serializers.RelatedField(many=True)
 	class Meta:
 		model = Contacts
-		fields = ('name', 'avator', 'description', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy')
+		fields = ('name', 'avator', 'tags', 'data', 'links', 'description', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy')
 
 class ContactTagSerializer(serializers.ModelSerializer):
 	contactname = serializers.Field(source='name')
 	class Meta:
 		model = ContactTag
-		fields = ('contactname','tag', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy') 
+		fields = ('contact','tag', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy') 
 
 class ContactDataSerializer(serializers.ModelSerializer):
 	contactname = serializers.Field(source='name')
 	class Meta:
 		model = ContactData
-		fields = ('contactname','key', 'value', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy') 
+		fields = ('contact','key', 'value', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy') 
 
 class ContactLinkSerializer(serializers.ModelSerializer):
 	contactname = serializers.Field(source='name')
 	class Meta:
 		model = ContactLink
-		fields = ('contactname','entityType', 'entity', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy') 
+		fields = ('contact','entityType', 'entity', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy') 
