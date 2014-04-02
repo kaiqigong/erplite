9a8032eb3daac292ab554a5fa01cf1515061597a
lacusrinz <lacusrinz@gmail.com>
2014-04-02 23:42:39 +0800

    enable query

--- Server/Contacts/filters.py
+++ Server/Contacts/filters.py
@@ -9,6 +9,23 @@ class ContactsFilter(django_filters.FilterSet):
 	modifiedDate = django_filters.DateTimeFilter(name='modifiedDate')
 	modifiedBy = django_filters.CharFilter(name='modifiedBy', lookup_type='icontains')
 	tags = django_filters.CharFilter(name='tags__tag', lookup_type='icontains')
+	# surname givenname company department title phone mobile fax origin email address birthday region website qq weibo im 
+	surname = django_filters.CharFilter(name='data__surname', lookup_type='icontains')
+	givenname = django_filters.CharFilter(name='data__givenname', lookup_type='icontains')
+	department = django_filters.CharFilter(name='data__department', lookup_type='icontains')
+	title = django_filters.CharFilter(name='data__title', lookup_type='icontains')
+	phone = django_filters.CharFilter(name='data__phone', lookup_type='icontains')
+	mobile = django_filters.CharFilter(name='data__mobile', lookup_type='icontains')
+	fax = django_filters.CharFilter(name='data__fax', lookup_type='icontains')
+	origin = django_filters.CharFilter(name='data__origin', lookup_type='icontains')
+	email = django_filters.CharFilter(name='data__email', lookup_type='icontains')
+	address = django_filters.CharFilter(name='data__address', lookup_type='icontains')
+	birthday = django_filters.CharFilter(name='data__birthday', lookup_type='icontains')
+	region = django_filters.CharFilter(name='data__region', lookup_type='icontains')
+	website = django_filters.CharFilter(name='data__website', lookup_type='icontains')
+	qq = django_filters.CharFilter(name='data__qq', lookup_type='icontains')
+	weibo = django_filters.CharFilter(name='data__weibo', lookup_type='icontains')
+	im = django_filters.CharFilter(name='data__im', lookup_type='icontains')
 	class Meta:
 		model = Contacts
 		fields = ('name','description','tags','createdDate','createdBy','modifiedDate','modifiedBy',)
@@ -24,15 +41,6 @@ class ContactTagFilter(django_filters.FilterSet):
 		model = ContactTag
 		fields = ('contact','tag','createdDate','createdBy','modifiedDate','modifiedBy',)
 
-# class ContactDataFilter(django_filters.FilterSet):
-# 	contact = django_filters.CharFilter(name='contact__name', lookup_type='icontains')
-# 	createdDate = django_filters.DateTimeFilter(name='createdDate')
-# 	createdBy = django_filters.CharFilter(name='createdBy', lookup_type='icontains')
-# 	modifiedDate = django_filters.DateTimeFilter(name='modifiedDate')
-# 	modifiedBy = django_filters.CharFilter(name='modifiedBy', lookup_type='icontains')
-# 	class Meta:
-# 		model = ContactData
-# 		fields = ('contact','createdDate','createdBy','modifiedDate','modifiedBy',)
 
 class ContactLinkFilter(django_filters.FilterSet):
 	contact = django_filters.CharFilter(name='contact__name', lookup_type='icontains')
