45cbb06774dcea88520fb038a50ed655f15343e6
cagegong <cagegong@163.com>
2014-04-02 11:40:49 +0800

    Add contact data fields to serializers. Nest data into contactdetail

--- Server/Contacts/serializers.py
+++ Server/Contacts/serializers.py
@@ -7,25 +7,26 @@ class ContactsListSerializer(serializers.HyperlinkedModelSerializer):
 		model = Contacts
 		fields = ('id','url', 'name', 'avator', 'tags', 'description', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy')
 
+class ContactDataSerializer(serializers.ModelSerializer):
+	contactname = serializers.Field(source='contact.name')
+	class Meta:
+		model = ContactData
+		fields = ('id','contactname', 'contact','surname','givenname','company','department','title','phone','mobile','fax','origin','email','address','birthday','region','website','qq','weibo','im', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy') 
+
 class ContactsDetailSerializer(serializers.HyperlinkedModelSerializer):
 	tags = serializers.HyperlinkedRelatedField(many=True,view_name='contacttag-detail')
-	data = serializers.HyperlinkedRelatedField(many=True, view_name='contactdata-detail')
+	data = ContactDataSerializer(required=False)
 	links = serializers.HyperlinkedRelatedField(many=True, view_name='contactlink-detail')
 	class Meta:
 		model = Contacts
 		fields = ('id', 'name', 'avator', 'tags', 'data', 'links', 'description', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy')
+		depth = 1
 
 class ContactTagSerializer(serializers.ModelSerializer):
 	contactname = serializers.Field(source='contact.name')
 	class Meta:
 		model = ContactTag
 		fields = ('contact','contactname','tag', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy') 
-
-class ContactDataSerializer(serializers.ModelSerializer):
-	contactname = serializers.Field(source='contact.name')
-	class Meta:
-		model = ContactData
-		fields = ('contact','contactname','keyPair', 'valuePair', 'createdDate', 'createdBy', 'modifiedDate', 'modifiedBy') 
 
 class ContactLinkSerializer(serializers.ModelSerializer):
 	contactname = serializers.Field(source='contact.name')
