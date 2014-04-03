9a3c3096ad627d17f5299bc27834885d04c8d54f
lacusrinz <lacusrinz@gmail.com>
2014-04-03 22:25:05 +0800

    update filter

--- Server/Contacts/filters.py
+++ Server/Contacts/filters.py
@@ -29,27 +29,3 @@ class ContactsFilter(django_filters.FilterSet):
 	class Meta:
 		model = Contacts
 		fields = ('name','description','tags','createdDate','createdBy','modifiedDate','modifiedBy',)
-
-class ContactTagFilter(django_filters.FilterSet):
-	contact = django_filters.CharFilter(name='contact__name', lookup_type='icontains')
-	tag = django_filters.CharFilter(name='tag', lookup_type='icontains')
-	createdDate = django_filters.DateTimeFilter(name='createdDate')
-	createdBy = django_filters.CharFilter(name='createdBy', lookup_type='icontains')
-	modifiedDate = django_filters.DateTimeFilter(name='modifiedDate')
-	modifiedBy = django_filters.CharFilter(name='modifiedBy', lookup_type='icontains')
-	class Meta:
-		model = ContactTag
-		fields = ('contact','tag','createdDate','createdBy','modifiedDate','modifiedBy',)
-
-
-class ContactLinkFilter(django_filters.FilterSet):
-	contact = django_filters.CharFilter(name='contact__name', lookup_type='icontains')
-	entityType = django_filters.CharFilter(name='entityType')
-	entity = django_filters.CharFilter(name='entity', lookup_type='icontains')
-	createdDate = django_filters.DateTimeFilter(name='createdDate')
-	createdBy = django_filters.CharFilter(name='createdBy', lookup_type='icontains')
-	modifiedDate = django_filters.DateTimeFilter(name='modifiedDate')
-	modifiedBy = django_filters.CharFilter(name='modifiedBy', lookup_type='icontains')
-	class Meta:
-		model = ContactLink
-		fields = ('contact','entityType','entity','createdDate','createdBy','modifiedDate','modifiedBy',)
