95027d0bb1d93520831b2d933da33bea5c9d9120
lacusrinz <lacusrinz@gmail.com>
2014-04-14 22:44:05 +0800

    update

--- Server/Accounts/forms.py
+++ Server/Accounts/forms.py
@@ -25,4 +25,45 @@ class RegisterForm(forms.Form):
 class LoginForm(forms.Form):
     username=forms.CharField(label=_(u"昵称"),max_length=30,widget=forms.TextInput(attrs={'size': 20,}))
     password=forms.CharField(label=_(u"密码"),max_length=30,widget=forms.PasswordInput(attrs={'size': 20,}))
+
+
+class ChangepwdForm(forms.Form):
+    oldpassword = forms.CharField(
+        required=True,
+        label=u"原密码",
+        error_messages={'required': u'请输入原密码'},
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder':u"原密码",
+            }
+        ),
+    ) 
+    newpassword1 = forms.CharField(
+        required=True,
+        label=u"新密码",
+        error_messages={'required': u'请输入新密码'},
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder':u"新密码",
+            }
+        ),
+    )
+    newpassword2 = forms.CharField(
+        required=True,
+        label=u"确认密码",
+        error_messages={'required': u'请再次输入新密码'},
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder':u"确认密码",
+            }
+        ),
+     )
+    def clean(self):
+        if not self.is_valid():
+            raise forms.ValidationError(u"所有项都为必填项")
+        elif self.cleaned_data['newpassword1'] <> self.cleaned_data['newpassword2']:
+            raise forms.ValidationError(u"两次输入的新密码不一样")
+        else:
+            cleaned_data = super(ChangepwdForm, self).clean()
+        return cleaned_data
     
--- Server/Accounts/views.py
+++ Server/Accounts/views.py
@@ -76,3 +76,23 @@ def generate(request):
         print token
         return HttpResponse(token)
 
+# def changepwd(request):
+#     if request.method == 'GET':
+#         form = ChangepwdForm()
+#         return render_to_response('changepwd.html', RequestContext(request, {'form': form,}))
+#     else:
+#         form = ChangepwdForm(request.POST)
+#         if form.is_valid():
+#             username = request.user.username
+#             oldpassword = request.POST.get('oldpassword', '')
+#             user = auth.authenticate(username=username, password=oldpassword)
+#             if user is not None and user.is_active:
+#                 newpassword = request.POST.get('newpassword1', '')
+#                 user.set_password(newpassword)
+#                 user.save()
+#                 return render_to_response('index.html', RequestContext(request,{'changepwd_success':True}))
+#             else:
+#                 return render_to_response('changepwd.html', RequestContext(request, {'form': form,'oldpassword_is_wrong':True}))
+#         else:
+#             return render_to_response('changepwd.html', RequestContext(request, {'form': form,}))
+
