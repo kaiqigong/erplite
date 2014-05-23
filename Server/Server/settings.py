9d5136ca9e131d43d253c36cf0c9ef98cbef1367
lacusrinz <lacusrinz@gmail.com>
2014-05-23 14:10:51 +0800

    add exception handler

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -160,7 +160,8 @@ REST_FRAMEWORK = {
 
 	'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'EXCEPTION_HANDLER': 'Server.utils.custom_exception_handler'
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
