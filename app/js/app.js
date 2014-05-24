35cbc2604aad67981efcd12472892ce51d099a71
lacusrinz <lacusrinz@gmail.com>
2014-05-24 14:20:09 +0800

    add pictures

--- app/js/app.js
+++ app/js/app.js
@@ -6,8 +6,8 @@
 
   erpApp.constant('erpSettings', {
     apiHost: 'http://localhost:8000',
-    client_id: '6f5eda9670e48e6b4c97',
-    client_secret: '93f7d72e077c1f749377bd3c069909a81a0df02c'
+    client_id: '1b69e642e6cd7663b906',
+    client_secret: '858ef81b97c03fb3dc70b7eef43f224f63d4e48f'
   });
 
   erpApp.config([
