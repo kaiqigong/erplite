d192b4ac856db10f45195f6388ecff9eb4b80c26
lacusrinz <lacusrinz@gmail.com>
2014-07-05 21:44:10 +0800

    update file upload

--- Server/FileUpload/urls.py
+++ Server/FileUpload/urls.py
@@ -3,7 +3,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from Accounts import views
 
 urlpatterns = patterns('FileUpload.views',
-	url(r'^upload/$', 'upload_file',name="upload_file"),
 	url(r'^uploadToken/$', 'get_qiniu_uptoken',name="get_qiniu_uptoken"),
 	url(r'^downloadToken/$','get_qiniu_dntoken',name='get_qiniu_dntoken'),
 	url(r'^getSignedUrl/$','get_signed_7n_url',name='get_signed_7n_url')
